e2eb8392c0dd0a244cffd884246197fb0f4f335e
Konstantin-nik <78786301+Konstantin-nik@users.noreply.github.com>
2024-08-08 12:11:55 +0300

    deployment

--- config.py
+++ config.py
@@ -12,18 +12,18 @@ with open(PARAMS_YAML_DIR, 'rb') as f:
 #######################################################
 # Directories
 #######################################################
-RAW_DATA_DIR = configs.directories.data.raw
-PROCESSED_DATA_DIR = configs.directories.data.processed
-MODELS_DIR = configs.directories.model
-REPORTS_DIR = configs.directories.report
+RAW_DATA_DIR = configs["directories"]["data"]["raw"]
+PROCESSED_DATA_DIR = configs["directories"]["data"]["processed"]
+MODELS_DIR = configs["directories"]["model"]
+REPORTS_DIR = configs["directories"]["report"]
 
 #######################################################
 # File names
 #######################################################
-TAXI_DATA = configs.filenames.data.taxi
-ZONES_DATA = configs.filenames.data.zones
-TRAIN_DATA = configs.filenames.data.train
-VALIDATION_DATA = configs.filenames.data.validation
-TEST_DATA = configs.filenames.data.test
-MODEL = configs.filenames.model
-EVALUATION_REPORT = configs.filenames.evaluation_report
+TAXI_DATA = configs["filenames"]["data"]["taxi"]
+ZONES_DATA = configs["filenames"]["data"]["zones"]
+TRAIN_DATA = configs["filenames"]["data"]["train"]
+VALIDATION_DATA = configs["filenames"]["data"]["validation"]
+TEST_DATA = configs["filenames"]["data"]["test"]
+MODEL = configs["filenames"]["model"]
+EVALUATION_REPORT = configs["filenames"]["evaluation_report"]
