ca35c68a55793cc697e068f2ccee38b2281465a6
Konstantin-nik <78786301+Konstantin-nik@users.noreply.github.com>
2024-08-12 15:50:19 +0300

    fix api

--- backend/api.py
+++ backend/api.py
@@ -1,9 +1,11 @@
+import logging
+
 from fastapi import APIRouter, Depends
 from pydantic import BaseModel
-from typing import List
 from backend.model_executor import ModelExecutor
 
 router = APIRouter()
+logger = logging.getLogger(__name__)
 
 MODEL_PATH = "models/xgb.json"
 _model_executor = None
@@ -19,7 +21,7 @@ def get_model_executor() -> ModelExecutor:
 
 
 class PredictionRequest(BaseModel):
-    trip_id: int
+    trip_id: str
     request_datetime: str
     trip_distance: float
     PULocationID: int 
@@ -36,6 +38,7 @@ async def predict(
     request: PredictionRequest,
     model_executor: ModelExecutor = Depends(get_model_executor),
 ):  
+    logger.info(f"Predicting trip with id: {request.trip_id}")
     prediction = model_executor.predict(request)
     
     return PredictionResponse(prediction=prediction)
