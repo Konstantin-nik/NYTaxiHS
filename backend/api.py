e0c0bd3010f9410e05ac3ebc90250f79f56f9834
Konstantin-nik <78786301+Konstantin-nik@users.noreply.github.com>
2024-08-13 21:39:03 +0300

    add prediction logging

--- backend/api.py
+++ backend/api.py
@@ -1,14 +1,39 @@
 import logging
+import json
+
+import pandas as pd
+import numpy as np
 
 from fastapi import APIRouter, Depends
-from pydantic import BaseModel
 from backend.model_executor import ModelExecutor
+from backend.api_models import PredictionRequest, PredictionResponse
 
 router = APIRouter()
-logger = logging.getLogger(__name__)
 
 MODEL_PATH = "models/xgb.json"
 _model_executor = None
+logger = logging.getLogger(__name__)
+
+
+def log_trip_prediction(trip_id, features, prediction):
+    features_dict = dict()
+    for key, value in features.items():
+        if isinstance(value, np.ndarray):
+            features_dict[key] = value[0]
+        elif isinstance(value, pd.Series):
+            features_dict[key] = value.iloc[0]
+        else:
+            features_dict[key] = value
+
+    log_dict = {
+        "trip_id": trip_id,
+        "features": features_dict,
+        "prediction": float(prediction)
+    }
+
+    log_str = json.dumps(log_dict)
+
+    logger.info(f"Prediction_result: {log_str}")
 
 
 def get_model_executor() -> ModelExecutor:
@@ -20,26 +45,18 @@ def get_model_executor() -> ModelExecutor:
     return _model_executor
 
 
-class PredictionRequest(BaseModel):
-    trip_id: str
-    request_datetime: str
-    trip_distance: float
-    PULocationID: int 
-    DOLocationID: int
-    Airport: int
-
-
-class PredictionResponse(BaseModel):
-    prediction: float
-
-
 @router.post("/predict", response_model=PredictionResponse)
 async def predict(
     request: PredictionRequest,
     model_executor: ModelExecutor = Depends(get_model_executor),
 ):  
     logger.info(f"Predicting trip with id: {request.trip_id}")
+    features = model_executor.extract_features(request)
     prediction = model_executor.predict(request)
     
+    log_trip_prediction(
+        trip_id=request.trip_id,
+        features=features,
+        prediction=prediction,
+    )
     return PredictionResponse(prediction=prediction)
-    # return PredictionResponse(prediction=0.5)
--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI
 import uvicorn
+import logging
+
+from fastapi import FastAPI
 from backend.api import router as api_v1_router
 
 app = FastAPI(port=8000)
@@ -7,4 +9,5 @@ app = FastAPI(port=8000)
 app.include_router(api_v1_router, prefix="/api/v1")
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
     uvicorn.run(app, host="0.0.0.0")
