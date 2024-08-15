55f8f5ff7089bbffb9852dc5f08d7437304c0af5
Konstantin-nik <78786301+Konstantin-nik@users.noreply.github.com>
2024-08-15 10:40:26 +0300

    add logs

--- backend/api.py
+++ backend/api.py
@@ -51,6 +51,7 @@ async def predict(
     model_executor: ModelExecutor = Depends(get_model_executor),
 ):  
     logger.info(f"Predicting trip with id: {request.trip_id}")
+    logger.info(f"request:", request)
     features = model_executor.extract_features(request)
     prediction = model_executor.predict(request)
     
