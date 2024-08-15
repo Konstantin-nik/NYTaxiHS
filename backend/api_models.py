c5920278fa636f2d9210f5ea8d1a4cb23f2481e7
Konstantin-nik <78786301+Konstantin-nik@users.noreply.github.com>
2024-08-15 11:09:34 +0300

    update logs

--- backend/api_models.py
+++ backend/api_models.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+import logging
 
 
 class PredictionRequest(BaseModel):
@@ -13,12 +14,21 @@ class PredictionRequest(BaseModel):
         extra = "ignore"
 
     def __str__(self):
-        return f"\tTrip id: {self.trip_id}\n" + \
-            f"\request_datetime: {self.request_datetime}\n" + \
-            f"\trip_distance: {self.trip_distance}\n" + \
-            f"\PULocationID: {self.PULocationID}\n" + \
-            f"\DOLocationID: {self.DOLocationID}\n" + \
-            f"\Airport: {self.Airport}\n"
+        logger = logging.getLogger(__name__)
+
+        try:
+            line = (
+                f"\tTrip id: {self.trip_id}\n"
+                f"\tRequest datetime: {self.request_datetime}\n"
+                f"\tTrip distance: {self.trip_distance}\n"
+                f"\tPULocationID: {self.PULocationID}\n"
+                f"\tDOLocationID: {self.DOLocationID}\n"
+                f"\tAirport: {self.Airport}\n"
+            )
+            return line
+        except Exception as e:
+            logger.warning(f"Error formatting PredictionRequest: {e}")
+            return "Error formatting PredictionRequest"
 
 
 class PredictionResponse(BaseModel):
